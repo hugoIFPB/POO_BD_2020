7136a3a977579d64c6914eaa9080fe9c61374957
hugoIFPB <hugoff@gmail.com>
2021-03-09 12:59:29 +0000

    executei script de criação. Correção no script de criação das tabelas

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 let Sequelize = require("sequelize");
 
 let db = new Sequelize(
-    "postgres://dkhtiuhlixbnxh:ec791416b40dcd02e4a65a2807f6612e100f078565cb0c20289e035634cb0fae@ec2-54-159-175-113.compute-1.amazonaws.com:5432/d1gc0oa31nd9m6",
+    "",
     {
         dialect: "postgres",
         dialectOptions: {
