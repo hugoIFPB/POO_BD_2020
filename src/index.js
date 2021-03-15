93ff6c0907e3ce7e7840d44ba75863d0e3de6692
Hugo Feitosa de Figueirêdo <hugo.figueiredo@ifpb.edu.br>
2021-03-15 12:09:00 -0300

    alterando index para o heroku

--- src/index.js
+++ src/index.js
@@ -1,54 +1,23 @@
 require('dotenv').config()
-const { Op, Model } = require('sequelize');
-let db = require("./db");
-const Disciplina = require('./model/Disciplina');
-const DisciplinaLivro = require('./model/DisciplinaLivro');
-const Edicao = require('./model/Edicao');
+const express = require("express"); 
 const Livro = require('./model/Livro');
 
-async function sincronizar() {
-    await db.sync({force: true});
-}
+const app = express();
 
-async function inserir() {
-    let livro = {titulo: "Arquitetura MERN", autores: "Sicrano"};
-    livro = await Livro.create(livro);
-    Edicao.create({quantidade:3, LivroId: livro.id}); 
-    livro = await Livro.create({titulo: "Arquitetura PERN", autores: "Fulano"});
-    Edicao.create({quantidade:2, LivroId: livro.id}); 
-    livro = await Livro.create({titulo: "Arquitetura MEAN", autores: "Fulano"});
-    Edicao.create({quantidade:5, LivroId: livro.id}); 
-    livro = await Livro.create({titulo: "Javascript com Node", autores: "Beltrano"});
-    Edicao.create({quantidade:1, LivroId: livro.id}); 
-    livro = await Livro.create({titulo: "React com Hooks", autores: "Sicrano"});
-    Edicao.create({quantidade:2, LivroId: livro.id}); 
-}
+app.get("/", (req, res) => {
+    res.send("Valeu!!!!")
+});
 
-// 
-// inserir(a1);
+app.get("/livro/", async (req, res) => {
+    let livros = await Livro.findAll();
+    res.send(livros);
+});
 
-async function consultar() {
-    /*let livros =  await Livro.findAll({
-        where: {
-            titulo: {
-                [Op.like]: "Arquitetura%"
-            }
-        }
-    }
-    );
-    console.log(livros);
-    */
-    let livro = await Livro.findOne(
-        {
-            where: {
-                id:4
-            }
-        }
-    );
-    //let ed = Edicao.findOne();
-    //ed.getLivro();
-    console.log((await livro.getEdicoes()));
-}
-//sincronizar();
-//inserir();
-//consultar();
+app.post("livro", async (req, res) => {
+    let livros = await Livro.create({titulo:"Algum Livro", autores:"Autor do Livro"});
+    res.send(livros);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor escutando na porta ${process.env.PORT}`);
+})
